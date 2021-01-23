dacc0ae687857dfff20f30aa87f8aa932fe0530d
Josh Buckley <jgb4ub@virginia.edu>
2021-01-23 15:56:18 -0500

    site connection and test

--- ipo-app/src/main/java/com/jgb/app/App.java
+++ ipo-app/src/main/java/com/jgb/app/App.java
@@ -1,13 +1,32 @@
 package com.jgb.app;
 
-/**
- * Hello world!
- *
- */
+import java.io.IOException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        try {
+            // Here we create a document object and use JSoup to fetch the website
+            Document doc1 = Jsoup.connect("https://www.nasdaq.com/market-activity/ipos").get();
+            
+            Document doc2 = Jsoup.connect("https://finance.yahoo.com/calendar/ipo?from=2021-01-17&to=2021-01-23&day=2021-01-19").get();
+            
+            Document doc3 = Jsoup.connect(" https://www.marketwatch.com/tools/ipo-calendar").get();
+     
+            System.out.printf("Title: %s\n", doc1.title());/*,"\n ",doc2.title(),"\n ", doc3.title()*/
+            
+            System.out.printf("Title: %s\n", doc2.title());
+
+            System.out.printf("Title: %s\n", doc3.title());
+
+          } catch (IOException e) {
+            e.printStackTrace();
+          }
     }
 }
